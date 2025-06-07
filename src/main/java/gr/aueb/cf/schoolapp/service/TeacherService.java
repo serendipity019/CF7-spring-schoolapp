a3a4833a74d0a2a5e0074ce601e4d87a4aca89c5
serendipity019 <navagio09@gmail.com>
2025-06-07 23:47:16 +0300

    feat:we create TeacherService and saw how to make and save a file with the MultipartFile

---
+++ src/main/java/gr/aueb/cf/schoolapp/service/TeacherService.java
@@ -0,0 +1,85 @@
+package gr.aueb.cf.schoolapp.service;
+
+import gr.aueb.cf.schoolapp.core.exceptions.AppObjectAlreadyExists;
+import gr.aueb.cf.schoolapp.core.exceptions.AppObjectInvalidArgumentException;
+import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp.mapper.Mapper;
+import gr.aueb.cf.schoolapp.model.AttachmentEntity;
+import gr.aueb.cf.schoolapp.model.PersonalInfoEntity;
+import gr.aueb.cf.schoolapp.model.TeacherEntity;
+import gr.aueb.cf.schoolapp.repository.PersonalInfoRepository;
+import gr.aueb.cf.schoolapp.repository.TeacherRepository;
+import gr.aueb.cf.schoolapp.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class TeacherService {
+    private final static Logger LOGGER = LoggerFactory.getLogger(TeacherService.class);
+    private final TeacherRepository teacherRepository;
+    private final Mapper mapper;
+    private final UserRepository userRepository;
+    private final PersonalInfoRepository personalInfoRepository;
+
+    @Transactional(rollbackOn = {AppObjectAlreadyExists.class, IOException.class})
+    public TeacherReadOnlyDTO saveTeacher(TeacherInsertDTO teacherInsertDTO, MultipartFile amkaFile)
+        throws AppObjectAlreadyExists, AppObjectInvalidArgumentException, IOException {
+        if (userRepository.findByVat(teacherInsertDTO.user().vat()).isPresent()) {
+            throw new AppObjectAlreadyExists("User", "User with vat= " + teacherInsertDTO.user().vat() + " already exists.");
+        }
+
+        if (userRepository.findByUsername(teacherInsertDTO.user().username()).isPresent()) {
+            throw new AppObjectAlreadyExists("User", "User with username= " + teacherInsertDTO.user().username() + " already exists.");
+        }
+
+        // Saving the teacher cascades user & personal info
+        TeacherEntity teacher = mapper.mapToTeacherEntity(teacherInsertDTO);
+
+        saveAmkaFile(teacher.getPersonalInfo(), amkaFile);
+        TeacherEntity savedTeacher = teacherRepository.save(teacher);
+
+        return mapper.mapToTeacherReadOnlyDTO(savedTeacher);
+    }
+
+    public void saveAmkaFile(PersonalInfoEntity personalInfo, MultipartFile amkaFile) throws IOException {
+        if ( amkaFile == null || amkaFile.isEmpty()) return;
+
+        String originalFilename = amkaFile.getOriginalFilename();
+        String savedName = UUID.randomUUID().toString() + getFileExtension(originalFilename);
+
+        String uploadDirectory = "uploads/";
+        Path filePath = Paths.get(uploadDirectory + savedName);
+        Files.createDirectories(filePath.getParent());
+        Files.write(filePath, amkaFile.getBytes());
+
+        // Here we write the metadata into attachment
+        AttachmentEntity attachment = new AttachmentEntity();
+        attachment.setFilename(originalFilename);
+        attachment.setSavedName(savedName);
+        attachment.setFilePath(filePath.toString());
+        attachment.setContentType(amkaFile.getContentType());
+        attachment.setExtension(getFileExtension(originalFilename));
+
+        // We put it all above in personalInfo
+        personalInfo.setAmkaFile(attachment);
+
+
+    }
+
+    private String getFileExtension(String filename) {
+        if (filename == null || !filename.contains(".")) return "";
+        return filename.substring(filename.lastIndexOf("."));
+    }
+}
