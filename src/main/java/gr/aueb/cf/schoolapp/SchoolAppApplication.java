6d398772685f314c407b3e0a5a41509715b337ce
serendipity019 <navagio09@gmail.com>
2025-06-16 16:49:05 +0300

    Fix: We add @EnableJpaRepositories because without this don't recognize the beans while building the app.

--- src/main/java/gr/aueb/cf/schoolapp/SchoolAppApplication.java
+++ src/main/java/gr/aueb/cf/schoolapp/SchoolAppApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages="gr.aueb.cf.schoolapp.repository")
 public class SchoolAppApplication {
 
 	public static void main(String[] args) {
