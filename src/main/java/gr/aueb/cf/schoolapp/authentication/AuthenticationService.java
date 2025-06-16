0e09b46993c068752f7b3b5fde27c72306e21bf6
serendipity019 <navagio09@gmail.com>
2025-06-16 14:54:35 +0300

    Change: refactor the name

--- src/main/java/gr/aueb/cf/schoolapp/authentication/AuthenticationService.java
+++ src/main/java/gr/aueb/cf/schoolapp/authentication/AuthenticationService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class authenticationService {
+public class AuthenticationService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
