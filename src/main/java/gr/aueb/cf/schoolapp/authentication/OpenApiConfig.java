d3c52e98ee58ebe308cf3d08dc6473339f0ac4e8
serendipity019 <navagio09@gmail.com>
2025-06-16 15:01:09 +0300

    feat: we create OpenApiConfig file where used from Swagger to provide general info

---
+++ src/main/java/gr/aueb/cf/schoolapp/authentication/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package gr.aueb.cf.schoolapp.authentication;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("School API")
+                        .version("1.0.0")
+                        .description("Provides API for Schools with teachers")
+                        .contact(new Contact()
+                                .name("Admin")
+                                .email("admin@cf.gr")
+                                .url("https://cf.gr"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
